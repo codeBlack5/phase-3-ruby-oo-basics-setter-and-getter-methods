3cfcec65b9c507d9321a6506c6c64e6a204aca04
Steve <stevekoech8@gmail.com>
2023-02-11 13:11:18 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+    #setter method
+    def name=(name)
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
 
+    #setter method
+    def job=(job)
+        @job = job
+    end
+    #getter method
+    def job
+        @job
+    end
 end
+kanye = Person.new
+kanye.name = "Kanye"
+kanye.job = "Artist"
+puts kanye.name
+puts kanye.job
